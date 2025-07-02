2f58a88cfd7be3fe24e26011b612047ce6e3d00b
Agustin <agustin@agustin>
2025-07-01 22:04:46 -0600

    Arreglando el error pasado

--- Menu/deleteCookies.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-        setcookie("nombre", "", time() - 3600, "/");
-        setcookie("boleta", "", time() - 3600, "/");
-        setcookie("tipo", "", time() - 3600, "/");
-        
-        header("Location: ./newMenu/main.php");
-
-?>
